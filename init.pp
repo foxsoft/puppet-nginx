418156211aef1a114702995790d79b7fc4891733
Omar Qureshi <omar@omarqureshi.net>
2011-10-20 10:26:35 +0100

    require passenger installed

--- init.pp
+++ init.pp
@@ -2,7 +2,8 @@ class nginx {
 
   exec{"install_nginx":
     command => "passenger-install-nginx-module --auto --auto-download --prefix=/opt/nginx",
-    creates => "/opt/nginx"
+    creates => "/opt/nginx",
+    require => Rvm_gem["ruby-1.9.2-p290@global/passenger"]
   }
 
 }
